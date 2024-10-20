a887211a11987738c9ba206cadb3f77f11045551
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-20 15:02:27 +0100

    SimpleFizzBuzzTests

---
+++ FizzBuzz/Implementation/SimpleFizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿namespace FizzBuzzPlus.Implementation;
+
+public static class SimpleFizzBuzz
+{
+    public static string[] FizzBuzzIt(int[] array, IFizzBuzzer fizzBuzzer, IReverser reverser, IOrderingExpert orderingExpert)
+    {
+        var ordering = orderingExpert.DetermineOrder(array);
+        if (ordering == Ordering.Descending)
+        {
+            return array.Select(fizzBuzzer.FizzBuzzIt).Select(reverser.Reverse).ToArray();
+        }
+        else
+        {
+            return array.Select(fizzBuzzer.FizzBuzzIt).ToArray();
+        }
+    }
+}
--- FizzBuzz/Tests/FizzBuzzPlusTests.cs
+++ FizzBuzz/Tests/FizzBuzzPlusTests.cs
@@ -4,34 +4,6 @@ namespace FizzBuzzPlus.Tests;
 
 public class FizzBuzzPlusTests
 {
-    [Theory]
-    [InlineData(new int[] { }, new string[] { })]
-
-    [InlineData(new[] { 1 }, new[] { "1" })]
-    [InlineData(new[] { 3 }, new[] { "fizz" })]
-    [InlineData(new[] { 5 }, new[] { "buzz" })]
-    [InlineData(new[] { 15 }, new[] { "fizz buzz" })]
-
-    [InlineData(new[] { 1, 2 }, new[] { "1", "2" })]
-    [InlineData(new[] { 1, 3 }, new[] { "1", "fizz" })]
-    [InlineData(new[] { 1, 5 }, new[] { "1", "buzz" })]
-    [InlineData(new[] { 1, 15 }, new[] { "1", "fizz buzz" })]
-
-    [InlineData(new[] { 2, 1 }, new[] { "2", "1" })]
-    [InlineData(new[] { 3, 1 }, new[] { "zzif", "1" })]
-    [InlineData(new[] { 5, 1 }, new[] { "zzub", "1" })]
-    [InlineData(new[] { 15, 1 }, new[] { "zzub zzif", "1" })]
-
-    [InlineData(new[] { 2, 1, 0 }, new[] { "2", "1", "zzub zzif" })]
-    [InlineData(new[] { 22, 13 }, new[] { "22", "13" })]
-    public void SimpleTests(int[] numbers, string[] expected)
-    {
-        var fizzBuzzer = new FizzBuzzer();
-        var reverser = new StringReverser();
-        var orderingExpert = new OrderingExpert();
-        var results = SimpleFizzBuzz(numbers, fizzBuzzer, reverser, orderingExpert);
-        results.Should().Equal(expected);
-    }
 
     [Theory]
     // zero elements
@@ -88,60 +60,4 @@ public class FizzBuzzPlusTests
         }
     }
 
-    [Theory]
-    [InlineData(Ordering.AscendingOrUnknown, new[] { "one", "two" })]
-    [InlineData(Ordering.Descending, new[] { "one-reversed", "two-reversed" })]
-    public void SimpleFizzBuzz_ReversesIfDescending(Ordering ordering, string[] expected)
-    {
-        var fizzBuzzer = Substitute.For<IFizzBuzzer>();
-        fizzBuzzer.FizzBuzzIt(1).Returns("one");
-        fizzBuzzer.FizzBuzzIt(2).Returns("two");
-
-        var reverser = Substitute.For<IReverser>();
-        reverser.Reverse("one").Returns("one-reversed");
-        reverser.Reverse("two").Returns("two-reversed");
-
-        var orderingExpert = Substitute.For<IOrderingExpert>();
-        orderingExpert.DetermineOrder(default!).ReturnsForAnyArgs(ordering);
-
-        var results = SimpleFizzBuzz([1, 2], fizzBuzzer, reverser, orderingExpert);
-
-        results.Should().Equal(expected);
-    }
-
-    [Theory]
-    [InlineData(new int[] { }, new string[] { })]
-    [InlineData(new int[] { 1 }, new string[] { "1" })]
-    [InlineData(new int[] { 1, 2 }, new string[] { "1", "2" })]
-    [InlineData(new int[] { 1, 2, 3 }, new string[] { "1", "2", "3" })]
-    public void SimpleFizzBuzz_ReturnsArrayWithSameLengthInSameOrderOfElements(int[] numbers, string[] expected)
-    {
-        var fizzBuzzer = Substitute.For<IFizzBuzzer>();
-        // Return the input number as a string
-        fizzBuzzer.FizzBuzzIt(default!).ReturnsForAnyArgs(c => c.Arg<int>().ToString());
-
-        var reverser = Substitute.For<IReverser>();
-        // Return the input string as is
-        reverser.Reverse(default!).ReturnsForAnyArgs(c => c.Arg<string>());
-
-        var orderingExpert = Substitute.For<IOrderingExpert>();
-        orderingExpert.DetermineOrder(default!).ReturnsForAnyArgs(Ordering.AscendingOrUnknown);
-
-        var results = SimpleFizzBuzz(numbers, fizzBuzzer, reverser, orderingExpert);
-
-        results.Should().Equal(expected);
-    }
-
-    private static string[] SimpleFizzBuzz(int[] array, IFizzBuzzer fizzBuzzer, IReverser reverser, IOrderingExpert orderingExpert)
-    {
-        var ordering = orderingExpert.DetermineOrder(array);
-        if (ordering == Ordering.Descending)
-        {
-            return array.Select(fizzBuzzer.FizzBuzzIt).Select(reverser.Reverse).ToArray();
-        }
-        else
-        {
-            return array.Select(fizzBuzzer.FizzBuzzIt).ToArray();
-        }
-    }
 }
