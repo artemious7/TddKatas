94ce7f7e05d77b5ac6f691e0dc00bab4aa923213
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-23 16:37:55 +0100

    FizzBuzzPy: green for 5

--- FizzBuzzPy/FizzBuzz.py
+++ FizzBuzzPy/FizzBuzz.py
@@ -1,4 +1,6 @@
 def FizzBuzzIt(input: int):
     if input == 3:
         return 'Fizz'
+    if input == 5:
+        return 'Buzz'
     return str(input)
--- FizzBuzzPy/test_FizzBuzz.py
+++ FizzBuzzPy/test_FizzBuzz.py
@@ -1,4 +1,3 @@
-
 import unittest
 
 from FizzBuzz import FizzBuzzIt
@@ -9,6 +8,9 @@ class FizzBuzzTests(unittest.TestCase):
 
     def test_3_Returns_Fizz(self):
         self.assertEqual('Fizz', FizzBuzzIt(3))
+
+    def test_5_Returns_Fizz(self):
+        self.assertEqual('Buzz', FizzBuzzIt(5))
 
 if __name__ == '__main__':
     unittest.main()
