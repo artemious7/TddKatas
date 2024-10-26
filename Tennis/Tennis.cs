46e5411a27c662595d758171c238de0f6fd3545a
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-26 22:56:29 +0100

    Tennis: WhenGameStarts_ThenLoveLove

--- Tennis/Tennis.cs
+++ Tennis/Tennis.cs
@@ -2,5 +2,10 @@
 
 internal class Tennis
 {
+    public string ScoreDescription { get; } = "love-love";
 
+    internal void StartGame()
+    {
+
+    }
 }
--- Tennis/TennisTests.cs
+++ Tennis/TennisTests.cs
@@ -1,10 +1,15 @@
+using FluentAssertions;
 namespace Tennis;
 
 public class TennisTests
 {
     [Fact]
-    public void Test1()
+    public void WhenGameStarts_ThenLoveLove()
     {
+        var sut = new Tennis();
 
+        sut.StartGame();
+
+        sut.ScoreDescription.Should().Be("love-love");
     }
 }
