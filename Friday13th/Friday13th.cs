5f9302718ac8bf18d5409762454489037ac8dbfa
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-26 21:29:10 +0100

    Friday13th: refactor

--- Friday13th/Friday13th.cs
+++ Friday13th/Friday13th.cs
@@ -5,10 +5,10 @@ internal class Friday13th
     internal DayOfWeek GetMostFrequentDayOfWeek(DateOnly startDate, DateOnly endDate)
     {
         Dictionary<DayOfWeek, int> counts = [];
-        var startThe13Th = new DateOnly(startDate.Year, startDate.Month, 13);
-        var endThe13Th = new DateOnly(endDate.Year, endDate.Month, 13);
-        counts[startThe13Th.DayOfWeek] = 1;
-        counts[endThe13Th.DayOfWeek] = 1;
+        for (var currentDate = new DateOnly(startDate.Year, startDate.Month, 13); currentDate < endDate; currentDate = currentDate.AddMonths(1))
+        {
+            counts[currentDate.DayOfWeek] = 1;
+        }
         return counts.MaxBy(r => r.Value).Key;
     }
 }
