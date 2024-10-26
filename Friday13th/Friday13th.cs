87084fec022171a445065031a3de28abd579fd0e
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-26 21:36:27 +0100

    Friday13th: most frequent day of week

--- Friday13th/Friday13th.cs
+++ Friday13th/Friday13th.cs
@@ -5,9 +5,13 @@ internal class Friday13th
     internal DayOfWeek GetMostFrequentDayOfWeek(DateOnly startDate, DateOnly endDate)
     {
         Dictionary<DayOfWeek, int> counts = [];
+        for (DayOfWeek day = DayOfWeek.Sunday; day <= DayOfWeek.Saturday; day++)
+        {
+            counts[day] = 0;
+        }
         for (var currentDate = new DateOnly(startDate.Year, startDate.Month, 13); currentDate < endDate; currentDate = currentDate.AddMonths(1))
         {
-            counts[currentDate.DayOfWeek] = 1;
+            counts[currentDate.DayOfWeek]++;
         }
         return counts.MaxBy(r => r.Value).Key;
     }
--- Friday13th/Friday13thTests.cs
+++ Friday13th/Friday13thTests.cs
@@ -2,20 +2,21 @@ namespace Friday13th;
 
 public class Friday13thTests
 {
-    public static readonly object[][] Data = 
+    public static readonly object[][] Data =
     [
-        [new DateOnly(2024, 10, 1), DayOfWeek.Sunday],
-        [new DateOnly(2024, 9, 1), DayOfWeek.Friday],
+        [new DateOnly(2024, 10, 1), new object[] { DayOfWeek.Sunday }],
+        [new DateOnly(2024, 09, 1), new object[] { DayOfWeek.Sunday, DayOfWeek.Friday }],
+        [new DateOnly(2024, 03, 1), new object[] { DayOfWeek.Saturday }],
     ];
 
     [Theory]
     [MemberData(nameof(Data))]
-    public void TestMonths(DateOnly startDate, DayOfWeek expected)
+    public void TestMonths(DateOnly startDate, DayOfWeek[] acceptible)
     {
         var sut = new Friday13th();
 
         DayOfWeek mostFrequentDayOfWeek = sut.GetMostFrequentDayOfWeek(startDate, new DateOnly(2024, 10, 31));
 
-        Assert.Equal(expected, mostFrequentDayOfWeek);
+        Assert.Contains(mostFrequentDayOfWeek, acceptible);
     }
 }
