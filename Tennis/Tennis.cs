91967b674094f241a9e5293f2efae4472ce39de7
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-26 23:02:18 +0100

    refactor

--- Tennis/Tennis.cs
+++ Tennis/Tennis.cs
@@ -2,10 +2,24 @@
 
 internal class Tennis
 {
-    public string ScoreDescription { get; } = "love-love";
+    private GameScore Score = new GameScore();
+    public string ScoreDescription => Score.ToString();
 
     internal void StartGame()
     {
 
+    }
+}
+
+internal record struct GameScore(PlayerPoints ServerPoints, PlayerPoints OpponentPoints)
+{
+    public override string ToString() => $"{ServerPoints}-{OpponentPoints}";
+}
+
+internal record struct PlayerPoints(int Points)
+{
+    public override string ToString()
+    {
+        return Points == 0 ? "love" : "";
     }
 }
