3a0aa7c32c69863d8c9a53c927026698436d0527
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-30 21:08:12 +0000

    refactor

--- DiversionPy/Diversion.py
+++ DiversionPy/Diversion.py
@@ -3,9 +3,7 @@ def answer(sequenceLength: int):
     def permutations():
         if sequenceLength == 0:
             return ['']
-        if sequenceLength == 1:
-            return [format(0, "b"), format(1, "b")]
-        if sequenceLength == 2:
-            return [format(0, "b"), format(1, "b"), format(2, "b"), format(3, "b")]
+        if sequenceLength in (1, 2):
+            return [format(x, 'b') for x in range(2 ** sequenceLength)]
 
     return len([permutation for permutation in permutations() if '11' not in permutation])
