0970a8996378992ecddcb69206d53be047b40867
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-30 20:42:39 +0000

    DiversionPy: test_1

--- DiversionPy/Diversion.py
+++ DiversionPy/Diversion.py
@@ -2,3 +2,5 @@ def Answer(sequenceLength: int):
     if sequenceLength == 0:
         expectedNumbersWithout2AdjacentOnes = 1
         return expectedNumbersWithout2AdjacentOnes
+    if sequenceLength == 1:
+        return 2
--- DiversionPy/test_Diversion.py
+++ DiversionPy/test_Diversion.py
@@ -5,5 +5,8 @@ class DiversionTests(unittest.TestCase):
     def test_0(self):
         self.assertEqual(Diversion.Answer(0), 1)
 
+    def test_1(self):
+        self.assertEqual(Diversion.Answer(1), 2)
+
 if __name__ == '__main__':
     unittest.main()
