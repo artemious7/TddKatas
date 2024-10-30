809c93346b7bd80c6186f2454acdcbb2b16dcbdd
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-30 21:57:35 +0000

    DivisionPy: prove relationship to fibonacci number

--- DiversionPy/Diversion.py
+++ DiversionPy/Diversion.py
@@ -1,8 +1,88 @@
 def naive_approach(sequenceLength: int):
 
-    def permutations():
+    def all_permutations():
         if sequenceLength == 0:
-            return ['']
-        return [format(x, 'b') for x in range(2 ** sequenceLength)]
+            return [""]
+        return [format(x, "b") for x in range(2**sequenceLength)]
 
-    return len([permutation for permutation in permutations() if '11' not in permutation])
+    return len(
+        [permutation for permutation in all_permutations() if "11" not in permutation]
+    )
+
+
+def proof_step1(sequenceLength: int):
+
+    def permutations(sequenceLength: int):
+        if sequenceLength == 0:
+            return [""]
+        if sequenceLength == 1:
+            return ["0", "1"]
+
+        return [("0" + x) for x in permutations(sequenceLength - 1)] + [
+            ("1" + x) for x in permutations(sequenceLength - 1) if x[0] != "1"
+        ]
+
+    return len(permutations(sequenceLength))
+
+
+def proof_step2(sequenceLength: int):
+
+    def permutations(sequenceLength: int):
+        if sequenceLength == 0:
+            return [""]
+        if sequenceLength == 1:
+            return ["0", "1"]
+
+        return [("0" + x) for x in permutations(sequenceLength - 1)] + [
+            ("1" + x) for x in permutations(sequenceLength - 2)
+        ]
+
+    return len(permutations(sequenceLength))
+
+
+def proof_step3(sequenceLength: int):
+
+    def permutations(sequenceLength: int):
+        if sequenceLength == 0:
+            return [""]
+        if sequenceLength == 1:
+            return ["0", "1"]
+
+        return permutations(sequenceLength - 1) + permutations(sequenceLength - 2)
+
+    return len(permutations(sequenceLength))
+
+
+def proof_step4(sequenceLength: int):
+
+    def permutations_count(sequenceLength: int):
+        if sequenceLength == 0:
+            return len([""])
+        if sequenceLength == 1:
+            return len(["0", "1"])
+
+        return permutations_count(sequenceLength - 1) + permutations_count(
+            sequenceLength - 2
+        )
+
+    return permutations_count(sequenceLength)
+
+
+def proof_step5(sequenceLength: int):
+
+    def permutations_count(sequenceLength: int):
+        if sequenceLength <= 0:
+            return 1
+
+        return permutations_count(sequenceLength - 1) + permutations_count(
+            sequenceLength - 2
+        )
+
+    return permutations_count(sequenceLength)
+
+
+def fibonacci(n: int):
+    if n <= 0:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
--- DiversionPy/test_Diversion.py
+++ DiversionPy/test_Diversion.py
@@ -1,9 +1,16 @@
 import unittest
 import Diversion
 
+
 class DiversionTests(unittest.TestCase):
     def execute_test(self, input: int, expected: int):
         self.assertEqual(Diversion.naive_approach(input), expected)
+        self.assertEqual(Diversion.proof_step1(input), expected)
+        self.assertEqual(Diversion.proof_step2(input), expected)
+        self.assertEqual(Diversion.proof_step3(input), expected)
+        self.assertEqual(Diversion.proof_step4(input), expected)
+        self.assertEqual(Diversion.proof_step5(input), expected)
+        self.assertEqual(Diversion.fibonacci(input), expected)
 
     def test_0(self):
         self.execute_test(0, 1)
@@ -23,5 +30,6 @@ class DiversionTests(unittest.TestCase):
     def test_10(self):
         self.execute_test(10, 144)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
