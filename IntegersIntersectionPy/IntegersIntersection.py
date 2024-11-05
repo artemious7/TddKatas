897d8bceb0991bea133e67a869f6bef0a1c1d531
Artemious <16724889+artemious7@users.noreply.github.com>
2024-11-05 23:03:06 +0000

    UseSetForResults

--- IntegersIntersectionPy/IntegersIntersection.py
+++ IntegersIntersectionPy/IntegersIntersection.py
@@ -1,7 +1,15 @@
-def IntegersIntersection(a: list, b: list):
+def DoublyNestedLoop(a: list, b: list):
     results = []
     for a_item in a:
         for b_item in b:
             if a_item == b_item and a_item not in results:
                 results.append(a_item)
     return results
+
+def UseSetForResults(a: list, b: list):
+    results = set()
+    for a_item in a:
+        for b_item in b:
+            if a_item == b_item:
+                results.add(a_item)
+    return list(results)
--- IntegersIntersectionPy/test_IntegersIntersection.py
+++ IntegersIntersectionPy/test_IntegersIntersection.py
@@ -1,15 +1,20 @@
 import unittest
-from IntegersIntersection import IntegersIntersection
+from IntegersIntersection import DoublyNestedLoop, UseSetForResults
 
 class IntegersIntersectionTests(unittest.TestCase):
+    def assertAllMethods(self, a: list, b: list, expected: list):
+        self.assertEqual(DoublyNestedLoop(a, b), expected)
+        self.assertEqual(UseSetForResults(a, b), expected)
+
     def test_empty(self):
-        self.assertEqual(IntegersIntersection([], []), [])
+        self.assertAllMethods([], [], [])
+        self.assertAllMethods([], [], [])
 
     def test_no_intersection(self):
-        self.assertEqual(IntegersIntersection([2], [1]), [])
+        self.assertAllMethods([2], [1], [])
 
     def test_intersection_for_1_1_2_and_1_is_1(self):
-        self.assertEqual(IntegersIntersection([1, 1, 2], [1]), [1])
+        self.assertAllMethods([1, 1, 2], [1], [1])
 
 if __name__ == '__main__':
     unittest.main()
