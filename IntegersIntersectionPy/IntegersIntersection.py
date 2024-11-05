d689317543b2b68a6e4f48b3a0b997344938eefc
Artemious <16724889+artemious7@users.noreply.github.com>
2024-11-05 22:52:45 +0000

    test_intersection_for_1_1_2_and_1_is_1

--- IntegersIntersectionPy/IntegersIntersection.py
+++ IntegersIntersectionPy/IntegersIntersection.py
@@ -1,2 +1,7 @@
-def IntegersIntersection(a, b):
-    return a
+def IntegersIntersection(a: list, b: list):
+    results = []
+    for a_item in a:
+        for b_item in b:
+            if a_item == b_item and a_item not in results:
+                results.append(a_item)
+    return results
--- IntegersIntersectionPy/test_IntegersIntersection.py
+++ IntegersIntersectionPy/test_IntegersIntersection.py
@@ -5,5 +5,11 @@ class IntegersIntersectionTests(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(IntegersIntersection([], []), [])
 
+    def test_no_intersection(self):
+        self.assertEqual(IntegersIntersection([2], [1]), [])
+
+    def test_intersection_for_1_1_2_and_1_is_1(self):
+        self.assertEqual(IntegersIntersection([1, 1, 2], [1]), [1])
+
 if __name__ == '__main__':
     unittest.main()
